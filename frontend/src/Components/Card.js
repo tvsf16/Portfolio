c80a5d27114a210c8880d2c550cf8e45d7abc2bb
tvsf16 <438102939@student.ksu.edu.sa>
2021-11-24 09:47:12 +0300

    page shows rick&morty charecter's information using fetch.

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -2,31 +2,31 @@ import { useState,useEffect } from "react";
 import './Card.css';
 
 function Card() {
-    const [Data,setData]= useState([]);
+    const [Data1,setData1]= useState([]);
     const [Item]=useState([
       {name:'CardNumberOne', description:'this is just a test'},
       {name:'CardNumberTwo', description:'this is just a test'},
       {name:'CardNumberThree', description:'this is just a test'}
     ]);
   useEffect(() => {
-   //   fetch("https://rickandmortyapi.com/api/character")
- // .then(response => response.json())
-  //.then(data => console.log(data.name));
-  //setData(data.name);
-        
+      fetch("https://rickandmortyapi.com/api/character")
+  .then(response => response.json())
+  .then(data => {console.log(data.results);
+    setData1(data.results);});
   },[]);
 
     return (
       <div className="container">
-      {Item.map((el) => {
+      {Data1.map((el) => {
         return (
-        <div className="card">
+        <div className="card" style={{backgroundImage: 'url('+el.image+')'}}>
           <div className="card-content">
             <h2 className="card-title">{el.name}</h2>
-            <p classNam="card-descrition">
-              {el.description}hi guys today i will talk about.
+            <p className="card-descrition">
+            species: {el.species}<br/>
+            status: {el.status}
             </p>
-            <a href="#" className="button">Learn More</a>
+            <a href={el.episode[0]} className="button">Learn More</a>
           </div>
         </div>
         );})} 
